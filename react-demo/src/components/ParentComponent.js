76ae0d81a1f9e5dae07f765a4b9b72dad791444b
EruRoraito <joshcruz2028@gmail.com>
2022-06-06 22:58:03 +0800

    added some comments for instructions, (probably will forget those after this day)

--- react-demo/src/components/ParentComponent.js
+++ react-demo/src/components/ParentComponent.js
@@ -2,6 +2,8 @@ import { ChildComponent } from "./ChildComponent"
 
 export const ParentComponent = () =>
 {
+    //remove 'childParameter' parameter in function greetParent if trying to use the other button from ChildComponent, 
+    //then remove backtick symbol (`) and use qoutation marks, then remove ${childParemeter}
     const greetParent = (childParemeter) =>
     {
         alert(`hello parent ${childParemeter}`)
