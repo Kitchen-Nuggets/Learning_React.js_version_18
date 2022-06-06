656874dc4ecfa74041f6454e637cacc1439e8a80
EruRoraito <joshcruz2028@gmail.com>
2022-06-06 23:15:24 +0800

    notes again

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -9,7 +9,8 @@ import { ParentComponent } from './components/ParentComponent';
 //TO GENERATE REACT APP FROM SCRATCH, type in terminal "npx create-react-app (name of the folder); EXAMPLE: npx create-react-app curriculum-system
 //NOTE: CAPITAL LETTERS AND WHITE SPACES FOR NAMING OF FOLDER ARE NOT ALLOWED"
 
-//to run this, type "cd ./react-demo" in terminal then type "npm start"
+//to run this, type "cd ./name-of-the-folder-you-created"; EXAMPLE: cd ./curriculum-system
+//then after getting into the folder, type "npm start"; 
 
 function App() 
 {
