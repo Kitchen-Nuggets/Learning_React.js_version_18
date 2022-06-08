95a16672a3a43ebfe835df6b68fab7ce5a59b97a
EruRoraito <joshcruz2028@gmail.com>
2022-06-08 21:47:33 +0800

    comment again

--- react-demo/src/components/UserGreeting.js
+++ react-demo/src/components/UserGreeting.js
@@ -2,6 +2,9 @@ export const UserGreeting = () =>
 {
     const isLoggedIn = false
 
+    //question mark is like, is boolean 'isLoggedIn' true? 
+    //if true, output 'User' if false, output 'Guest'
+
     return(
         <div>
             <h1> WELCOME {isLoggedIn ? 'User' : 'Guest'}</h1>
