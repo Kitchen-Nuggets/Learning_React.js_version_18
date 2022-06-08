d291dba6e6f5ef34127951d312082c4e4f13503e
EruRoraito <joshcruz2028@gmail.com>
2022-06-08 21:29:05 +0800

    added sum notes

--- react-demo/src/components/ParentComponent.js
+++ react-demo/src/components/ParentComponent.js
@@ -3,6 +3,7 @@ import { ChildComponent } from "./ChildComponent"
 export const ParentComponent = () =>
 {
     //remove 'childParameter' parameter in function greetParent if trying to use the other button from ChildComponent, 
+
     //then remove backtick symbol (`) and use qoutation marks, then remove ${childParemeter}
     const greetParent = (childParemeter) =>
     {
@@ -11,3 +12,7 @@ export const ParentComponent = () =>
 
     return <ChildComponent greetHandler={greetParent}/>
 }
+
+//this is how you pass functions as props(properties) in react components
+
+//also, handle the parent and child communication
